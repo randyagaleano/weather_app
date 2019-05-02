1f21ae9fce9a9e954d18426387fd13b1881a8008
Randy Galeano <randyagaleano@gmail.com>
2019-05-02 12:55:13 -0400

    correctly looping through admin-created cities
    The function for looping through cities created by the admin is now working
    User input has yet to work, but will be in progress

--- weather/views.py
+++ weather/views.py
@@ -1,21 +1,29 @@
 import requests
 from django.shortcuts import render
-
+from .models import City
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=743150ad3eadea636901037ce17ec386'
     city = 'Atlanta'
 
-    res = requests.get(url.format(city)).json()
+    cities = City.objects.all()
 
-    city_weather = {
-        'city' : city,
-        'temperature' : res['main']['temp'],
-        'description' : res['weather'][0]['description'],
-        'icon': res['weather'][0]['icon'],
-    }
+    weather_data = []
 
-    print(city_weather)
+    for city in cities:
+    
+        res = requests.get(url.format(city)).json()
 
-    context = {'city_weather' : city_weather}
+        city_weather = {
+            'city' : city.name,
+            'temperature' : res['main']['temp'],
+            'description' : res['weather'][0]['description'],
+            'icon': res['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+        
+    print(weather_data)
+
+    context = {'weather_data' : weather_data}
     return render(request, 'weather/weather.html', context)
