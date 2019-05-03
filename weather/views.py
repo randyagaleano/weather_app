caa73c6c0b1215ba941d9278e858da1a5bfd74ed
Randy Galeano <randyagaleano@gmail.com>
2019-05-03 16:58:26 -0400

    input form being imported
    complete functionality in progress

--- weather/views.py
+++ weather/views.py
@@ -1,10 +1,17 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
+
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=743150ad3eadea636901037ce17ec386'
-    city = 'Atlanta'
+    # city = 'Atlanta'
+
+    if request.method == 'POST':
+        pass 
+
+    form = CityForm()
 
     cities = City.objects.all()
 
@@ -25,5 +32,5 @@ def index(request):
         
     print(weather_data)
 
-    context = {'weather_data' : weather_data}
+    context = {'weather_data' : weather_data, 'form' : form}
     return render(request, 'weather/weather.html', context)
